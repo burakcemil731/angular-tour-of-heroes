15ed190249a6cb2dd56a8cee7d7b3a5a6e1b5045
burakcemil731 <burakcemil731@gmail.com>
2020-12-28 03:04:56 +0100

    final

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 11, name: 'Dr. Schliebe' },
+    { id: 12, name: 'Herbert Heilig' },
+    { id: 13, name: 'Gabo Krummfingo' },
+    { id: 14, name: 'Burtybaem' },
+    { id: 15, name: 'Gelbfußnagel' },
+    { id: 16, name: 'Stitches' },
+    { id: 17, name: 'Thralls Vater' },
+    { id: 18, name: 'Mister Doktor' },
+    { id: 19, name: 'Prof. Böbbels' },
+    { id: 20, name: 'Gobbdi und Bobbek' }
+];
